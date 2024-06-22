2223a133628a6032d70b60e56d3ae643f219561d
imakhlaq <imakhlaqXD@gmail.com>
2024-06-22 19:23:45 +0530

    added env support

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,3 +1,13 @@
 /**
  * Load the environment variables in an object here.
  */
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  PORT: process.env.PORT || 5000,
+};
+
+export default config;
